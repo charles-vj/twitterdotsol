fab69ab9a9dd73433406245dae6bc9e5603c31b1
charles-vj <charles2019@iiitkottayam.ac.in>
2021-11-26 22:47:21 +0530

    adding smart contract

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,26 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
+import {
+  Program, Provider, web3
+} from '@project-serum/anchor';
+
+import idl from './idl.json';
+const { SystemProgram, Keypair } = web3;
+
+
+let baseAccount = Keypair.generate();
+
+
+const programID = new PublicKey(idl.metadata.address);
+
+
+const network = clusterApiUrl('devnet');
+
+
+const opts = {
+  preflightCommitment: "processed"
+}
 
 
 const TWEETS = [
@@ -13,6 +34,27 @@ function App() {
   const [walletAddress, setWalletAddress] = useState(null);
   const [inputValue, setInputValue] = useState('');
   const [tweetList, setTweetList] = useState([]);
+
+  const createTweetAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping")
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getTweetList();
+  
+    } catch(error) {
+      console.log("Error creating BaseAccount account:", error)
+    }
+  }
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -48,8 +90,8 @@ function App() {
 
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching Tweet list...');
-      setTweetList(TWEETS);
+      console.log('Fetching TWEET list...');
+      getTweetList()
     }
   }, [walletAddress]);
 
@@ -64,6 +106,21 @@ function App() {
     }
   };
 
+  const getTweetList = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      
+      console.log("Got the account", account)
+      setTweetList(account.tweetList)
+  
+    } catch (error) {
+      console.log("Error in getTweetList: ", error)
+      setTweetList(null);
+    }
+  }
+
   /*
    * We want to render this UI when the user hasn't connected
    * their wallet to our app yet.
@@ -72,29 +129,84 @@ function App() {
     <button className="wallet" onClick={connectWallet}>Connect Wallet</button>
   );
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      <div className="tweet-grid">
-        {tweetList.map(tweet => (
-          <div className="tw" key={tweet}>
-            <h1>{tweet}</h1>
+  const renderConnectedContainer = () => {
+    // If we hit this, it means the program account hasn't be initialized.
+    if (tweetList === null) {
+      return (
+        <div className="connected-container">
+          <button className="btn" onClick={createTweetAccount}>
+            Do One-Time Initialization For GIF Program Account
+          </button>
+        </div>
+      )
+    } 
+    // Otherwise, we're good! Account exists. User can submit GIFs.
+    else {
+      return(
+        <div className="connected-container">
+          <div class="tweet">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              tweet();
+            }}
+          >
+            <textarea placeholder="Wassup!!" name="" id="" cols="80" rows="8" value={inputValue} onChange={onInputChange}></textarea>
+            
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </form>
           </div>
-        ))}
-      </div>
-    </div>
-  );
+          <div className="tweet-grid">
+            
+            {tweetList.map((item, index) => (
+              <div className="tw" key={index}>
+                <h1>{item.tweetLink}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      )
+    }
+  }
 
   const onInputChange = (event) => {
     const { value } = event.target;
     setInputValue(value);
   };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
   const tweet = async () => {
-    if (inputValue.length > 0) {
-      console.log('Tweet content:', inputValue);
-    } else {
-      console.log('Empty input. Try again.');
+    if (inputValue.length === 0) {
+      console.log("No tweet given!")
+      return
     }
+    console.log('Tweet:', inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+  
+      await program.rpc.addTweet(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      console.log("Tweet successfully sent to program", inputValue)
+  
+      await getTweetList();
+    } catch (error) {
+      console.log("Error sending TWEET:", error)
+    }
+    
   };
 
   return (
@@ -109,16 +221,7 @@ function App() {
         <div class="secondary-text">Hey there! This is Charles. This is a wall of tweets made on the solana blockchain. Do remember to connect your wallet or else you won't be able to tweet. Tell me what's happening</div>
         
       </div>
-      <div class="tweet">
-        <form action=""
-          onSubmit={(event) => {
-            event.preventDefault();
-            tweet();
-          }}>
-          <textarea placeholder="Wassup!!" name="" id="" cols="80" rows="8" value={inputValue} onChange={onInputChange}></textarea>
-          <button className="btn" type="submit">Tweet</button>
-        </form>
-      </div>
+      
     </div>
     <div className="tweets">
       <div class="tweet-header"><h1>Wall of tweets</h1></div>
